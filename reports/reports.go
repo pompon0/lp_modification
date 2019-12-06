4f5237c77a437f6c8b9cb4d924376046a9e26a0a
Grzegorz Prusak <pompon.pompon@gmail.com>
2019-12-06 11:47:29 +0100

    made reports sort by case name, print unsolved cases

--- reports/reports.go
+++ reports/reports.go
@@ -59,26 +59,35 @@ func summary(ctx context.Context) error {
   report,err := problems.ReadReport(*reportPath)
   if err!=nil { return fmt.Errorf("problems.ReadReport(report_path=%q): %v",*reportPath,err) }
   stats := NewStats()
+  fmt.Printf("labels = %v\n",report.Labels)
+  fmt.Printf("commit = %s\n",report.Commit)
+  solved := 0
   for _,c := range report.Cases {
     if c.GetOutput().GetProof()==nil { continue }
+    solved += 1
     d,err := ptypes.Duration(c.Duration)
     if err!=nil { log.Printf("ptypes.Duration(%q): %v",c.Name,err); continue }
     stats.Add(c.Output.Cost,Rec{1,c.Output.ContinuationCount,d})
   }
+  fmt.Printf("solved %d/%d\n",solved,len(report.Cases))
   fmt.Printf("%v",stats)
 
   sort.Slice(report.Cases, func(i,j int) bool {
-    ad,err := ptypes.Duration(report.Cases[i].Duration)
+    /*ad,err := ptypes.Duration(report.Cases[i].Duration)
     if err!=nil { panic("ptypes.Duration()"); }
     bd,err := ptypes.Duration(report.Cases[j].Duration)
     if err!=nil { panic("ptypes.Duration()"); }
-    return ad < bd
+    return ad < bd*/
+    return report.Cases[i].Name < report.Cases[j].Name
   })
   for _,c := range report.Cases {
-    if c.GetOutput().GetProof()==nil { continue }
-    d,err := ptypes.Duration(c.Duration)
-    if err!=nil { log.Printf("ptypes.Duration(%q): %v",c.Name,err); continue }
-    fmt.Printf("%20q : %05.2f cost=%3d cont=%d\n",c.Name,d.Seconds(),c.Output.Cost,c.Output.ContinuationCount)
+    if c.GetOutput().GetProof()==nil {
+      fmt.Printf("%20q : UNSOLVED\n",c.Name)
+    } else {
+      d,err := ptypes.Duration(c.Duration)
+      if err!=nil { log.Printf("ptypes.Duration(%q): %v",c.Name,err); continue }
+      fmt.Printf("%20q : %05.2f cost=%3d cont=%d\n",c.Name,d.Seconds(),c.Output.Cost,c.Output.ContinuationCount)
+    }
   }
   return nil
 }

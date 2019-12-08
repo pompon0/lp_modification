ff79851e8d9d68cbdc2f52ac22af5a09549dc258
Grzegorz Prusak <pompon.pompon@gmail.com>
2019-12-08 13:45:58 +0100

    replaced profiling with more general prover running binary

---
+++ lazyparam_prover/tableau/run/run.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "context"
+  "log"
+  "fmt"
+  "flag"
+
+  "github.com/pompon0/tptp_benchmark_go/tool"
+  "github.com/pompon0/tptp_benchmark_go/problems"
+  "github.com/pompon0/tptp_benchmark_go/lazyparam_prover/tableau"
+  spb "github.com/pompon0/tptp_benchmark_go/tptp_parser/proto/solutions_go_proto"
+)
+
+var caseName = flag.String("case_name","","")
+
+func run(ctx context.Context) error {
+  p,err := problems.GetProblems(ctx)
+  if err!=nil { return fmt.Errorf("problems.GetProblems(): %v",err) }
+  tptp,ok := p[*caseName]
+  if !ok { return fmt.Errorf("case %q not found",*caseName) }
+  fof,err := tool.TptpToProto(ctx,tool.FOF,tptp)
+  if err!=nil { return fmt.Errorf("tool.TptpToProto(%q): %v",*caseName,err) }
+  cnf,err := tool.FOFToCNF(ctx,fof)
+  tptpCnfProblem,err := tool.ProtoToTptp(ctx,cnf)
+  if err!=nil { return fmt.Errorf("tool.ProtoToTptp(): %v",err) }
+  fmt.Printf("-- CNF BEGIN --\n%s-- CNF END--\n",tptpCnfProblem)
+
+  if err!=nil { return fmt.Errorf("tool.FOFToCNF(%q): %v",*caseName,err) }
+  out,err := tableau.Tableau(ctx,cnf,true)
+  if err!=nil { return fmt.Errorf("Tableau(%q): %v",*caseName,err) }
+  _,err = tool.ValidateProof(ctx,&spb.CNF{Problem:cnf,Proof:out.Proof})
+  if err!=nil { return fmt.Errorf("tool.Validate(%q): %v",*caseName,err) }
+
+  tptpProof,err := tool.ProtoToTptp(ctx,out.Proof)
+  if err!=nil { return fmt.Errorf("tool.ProtoToTptp(): %v",err) }
+  fmt.Printf("-- PROOF BEGIN --\n%s-- PROOF END--\n",tptpProof)
+
+  return nil
+}
+
+func main() {
+  flag.Parse()
+  if err:=run(context.Background()); err!=nil {
+    log.Fatalf("run(): %v",err)
+  }
+}
